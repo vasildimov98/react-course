e307b1f3032d3ebb7256beed4da88ab5979827da
Vasil Dimov <vasil.dimov@gtmhub.com>
2023-08-01 09:14:06 +0300

    React Course | Adding CSS animation

--- 13-animated-react/src/App.js
+++ 13-animated-react/src/App.js
@@ -4,15 +4,55 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  toggleBlock = () => {
+    this.setState((prevState) => {
+      return { showBlock: !prevState.showBlock };
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <button className="Button" onClick={this.toggleBlock}>
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={300} unmountOnExit>
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                opacity: state === "exiting" ? 0 : 1,
+                transition: "opacity 1s ease-out",
+                margin: "auto",
+              }}
+            />
+          )}
+        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
