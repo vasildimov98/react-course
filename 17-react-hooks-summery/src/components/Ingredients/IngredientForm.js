a8fdade91392a0daffd2f627a2fa6dd57095fede
Vasil Dimov <vasil.dimov@gtmhub.com>
2023-08-16 08:58:05 +0300

    React Course | React Summery On Hook

---
+++ 17-react-hooks-summery/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import "./IngredientForm.css";
+
+const IngredientForm = React.memo((props) => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={(event) => {
+                setAmount(event.target.value);
+              }}
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
